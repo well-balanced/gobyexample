53683cd0bd7c8a191f5466a022e0dd2b5bcd4c47
Woosik Kim <well-balanced@Woosikui-MacBookPro.local>
2021-08-22 02:44:25 +0900

    :memo: Update arraypkg description

--- arraypkg/array.go
+++ arraypkg/array.go
@@ -2,6 +2,24 @@ package arraypkg
 
 import "fmt"
 
+/*
+In Go, an array is a numbered sequence of elements of a specific length.
+
+Here we create an array a that will hold exactly 5 ints. The type of elements and length are both part of the array’s type. By default an array is zero-valued, which for ints means 0s.
+
+We can set a value at an index using the array[index] = value syntax, and get a value with array[index].
+
+The builtin len returns the length of an array.
+
+Use this syntax to declare and initialize an array in one line.
+
+Array types are one-dimensional, but you can compose types to build multi-dimensional data structures.
+
+Note that arrays appear in the form [v1 v2 v3 ...] when printed with fmt.Println.
+
+You’ll see slices much more often than arrays in typical Go. We’ll look at slices next.
+*/
+
 func PrintArrays() {
 	fmt.Println("\n-------------array------------")
 	var a [5]int
